63b143292a9b20a711e909c96582a900a1cf084b
wongpet5 <peterskwongut@gmail.com>
2016-11-26 21:05:32 -0500

    Shopping List able to add items
    Modified Shopping List to add an item.

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient'; 
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  isAdd = true; 
+  item: Ingredient; 
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) {
+
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(ingredient: Ingredient) {
+    if (!this.isAdd){
+
+    }
+    else {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    }
   }
 
 }
