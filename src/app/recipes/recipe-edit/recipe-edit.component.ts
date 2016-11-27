61aa2d9adcfdda70535a5fe7da591956891af722
wongpet5 <peterskwongut@gmail.com>
2016-11-27 00:57:48 -0500

    recipe-edit.component gets queryParam
    Track the edit/new functionality of recipe-edit by parsing the Query
    Param.

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs/Rx";
+
+import { RecipeService } from "../recipe.service"; 
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private recipeIndex: number; 
+  private subscription: Subscription; 
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit() {
+    let isNew = true; 
+
+    // +params['id'] converts id from a string value to a number value 
+    this.subscription = this.route.params.subscribe(
+      (params: any) => { 
+        if (params.hasOwnProperty('id')){
+          this.recipeIndex = +params['id']
+          isNew = false; 
+        } else {
+          isNew = true; 
+        }
+        console.log(isNew);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe(); 
   }
 
 }
